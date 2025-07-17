1cbc10b02b884499427382f7941ce06f6596109e
amanraj <raj.aman4001@gmail.com>
2025-07-17 15:24:21 +0530

    fix backend callback in authstore
    Signed-off-by: amanraj <raj.aman4001@gmail.com>

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const  axiosInstance = axios.create({
-    baseURL:import.meta.env.MODE =="development"? "https://pingly-server.onrender.com/" : "/api",
+    baseURL:import.meta.env.MODE =="development"? "http://localhost:5001/api" : "https://pingly-server.onrender.com/api",
     withCredentials: true,
 
 });
